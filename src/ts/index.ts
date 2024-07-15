3058d36803d3b85271f681429b8ce95a97d00d6e
Kirill Lavrov <lkv@myttk.ru>
2024-07-16 00:58:40 +0700

    added nav link target on page

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,18 @@
-import { initCasesSliders, initClipboardMail, initUploadFiles, sendDesignSupportRequest } from "./modules";
+import { initCasesSliders, initClipboardMail, initUploadFiles, initValidateForms, sendDesignSupportRequest } from "./modules";
+import { initAutoheightAextarea, initModals, initSwiperOverflow, initTextFields } from "./ui";
+import { initScrollbar } from "./utils";
 
 document.addEventListener("DOMContentLoaded", initIndex);
 
 function initIndex() {
+  initTextFields();
+  initAutoheightAextarea();
+  initValidateForms();
+  initModals();
+
+  initSwiperOverflow();
+  initScrollbar();
+
   initUploadFiles();
   initCasesSliders();
   initClipboardMail();
--- src/ts/utils/init.ts
+++ src/ts/utils/init.ts
@@ -1,19 +1,10 @@
 import PerfectScrollbar from "perfect-scrollbar";
-import { initScrollTo, initValidateForms } from "../modules";
-import { initModals, initSwiperOverflow, initTextFields, initAutoheightAextarea } from "../ui";
-import { initScrollbar } from "./scrollbar";
+import { initScrollTo } from "../modules";
 import { initVh } from "./vh";
 
 export function initBase() {
-  initScrollbar();
   initScrollTo();
   initVh();
-  initSwiperOverflow();
-  initModals();
-  initTextFields();
-  initAutoheightAextarea();
-  initValidateForms();
-
 
   document.querySelectorAll('.custom-scroll').forEach(s =>
     window.store.customScrollbars.push(new PerfectScrollbar(s)));
